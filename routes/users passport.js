2d1ce72d2be22db45f1c7e70ecb911abd3b49ffc
giosal <GSalgado.developer@gmail.com>
2022-06-28 15:36:17 -0400

    Passport JWT

--- routes/users passport.js
+++ routes/users passport.js
@@ -1,9 +1,8 @@
-// Authenticate with passport & Json Web Token
+// Authenticate with passport
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 const passport = require('passport');
-const authenticate = require('../authenticate');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -31,10 +30,9 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'),(req, res) => {
-    const token = authenticate.getToken({_id: req.user._id});
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.json({success: true, token: token, status: 'You are successfully logged in!'}); 
+    res.json({success: true, status: 'You are successfully logged in!'}); 
  });
 
 router.get('/logout', (req, res, next) => {
